a0831de53cc49a8639e4953c988b6ba0fcb50c57
ramzi <dridi.ramzy@gmail.com>
2018-10-05 12:37:22 +0100

    crud hibernate

---
+++ crudhibernate/src/persistance/Dbsession.java
@@ -0,0 +1,34 @@
+package persistance;
+
+
+
+
+
+import org.hibernate.*;
+import org.hibernate.cfg.*;
+
+/**
+ * Startup Hibernate and provide access to the singleton SessionFactory
+ */
+public class Dbsession {
+
+  private static SessionFactory sessionFactory;
+
+  static {
+    try {
+       sessionFactory = new Configuration().configure().buildSessionFactory();
+    } catch (Throwable ex) {
+       throw new ExceptionInInitializerError(ex);
+    }
+  }
+
+  public static SessionFactory getSessionFactory() {
+      // Alternatively, we could look up in JNDI here
+      return sessionFactory;
+  }
+
+  public static void shutdown() {
+      // Close caches and connection pools
+      getSessionFactory().close();
+  }
+}
